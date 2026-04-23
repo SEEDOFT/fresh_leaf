fe3fa77d0fc08f2a7bf64d50e248056208514cdc
SEEDOFT <koyyotrabothffc001@gmail.com>
2026-04-23 15:29:04 +0700

    Fix Android build output path and ignore generated/private files

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,21 +5,18 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
+rootProject.layout.buildDirectory.value(rootProject.layout.projectDirectory.dir("../build"))
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
-}
+    project.layout.buildDirectory.value(rootProject.layout.buildDirectory.dir(project.name))
 
-subprojects {
     tasks.withType<org.gradle.api.tasks.compile.JavaCompile>().configureEach {
         options.compilerArgs.add("-Xlint:-options")
     }
+}
+
+subprojects {
+    project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
